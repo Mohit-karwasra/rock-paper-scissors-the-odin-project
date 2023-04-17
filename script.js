dd4c39a1bf8198b108d19acf8115f859b6514e12
Mohit-karwasra <97739963+Mohit-karwasra@users.noreply.github.com>
2023-04-17 15:21:41 +0530

    Transfer the game from console to window

--- script.js
+++ script.js
@@ -1,42 +1,80 @@
+const btn1 = document.getElementById("rock");
+const btn2 = document.getElementById("paper");
+const btn3 = document.getElementById("scissors");
+
 function getComputerChoice() {
 	const choices = ["rock", "paper", "scissors"];
 	return choices[Math.floor(Math.random() * choices.length)];
 }
 
+let playerWins = 0;
+let computerWins = 0;
+let totalRounds = 0;
+
 function playRound(playerSelection, computerSelection) {
-	if (playerSelection === "rock") {
-		if (computerSelection == "scissors") {
-			return "player";
-		} else if (computerSelection == "paper") {
-			return "computer";
-		} else {
-			return "draw";
+	totalRounds++;
+	if (playerWins < 5 && computerWins < 5) {
+		if (playerSelection === "rock") {
+			if (computerSelection == "scissors") {
+				playerWins++;
+				return `player : ${playerWins}/${totalRounds}`;
+			} else if (computerSelection == "paper") {
+				computerWins++;
+				return `computer : ${computerWins}/${totalRounds}`;
+			} else {
+				return "draw";
+			}
+		} else if (playerSelection === "paper") {
+			if (computerSelection == "scissors") {
+				computerWins++;
+				return `computer : ${computerWins}/${totalRounds}`;
+			} else if (computerSelection == "rock") {
+				playerWins++;
+				return `player : ${playerWins}/${totalRounds}`;
+			} else {
+				return "draw";
+			}
+		} else if (playerSelection === "scissors") {
+			if (computerSelection == "rock") {
+				computerWins++;
+				return `computer : ${computerWins}/${totalRounds}`;
+			} else if (computerSelection == "paper") {
+				playerWins++;
+				return `player : ${playerWins}/${totalRounds}`;
+			} else {
+				return "draw";
+			}
 		}
-	} else if (playerSelection === "paper") {
-		if (computerSelection == "scissors") {
-			return "computer";
-		} else if (computerSelection == "rock") {
-			return "player";
-		} else {
-			return "draw";
-		}
-	} else if (playerSelection === "scissors") {
-		if (computerSelection == "rock") {
-			return "computer";
-		} else if (computerSelection == "paper") {
-			return "player";
-		} else {
-			return "draw";
-		}
+	} else {
+		btn1.setAttribute("disabled", "");
+		btn2.setAttribute("disabled", "");
+		btn3.setAttribute("disabled", "");
+
+		return `${
+			playerWins < computerWins
+				? "computer wins : " + computerWins + "/" + totalRounds
+				: "player wins : " + playerWins + "/" + totalRounds
+		}`;
 	}
 }
 
-let numberOfRounds = prompt("Give number of rounds to play");
-
-for (let i = 0; i < numberOfRounds; i++) {
-	let playerSelection = prompt("Enter the choice - rock , paper or scissors").toLowerCase();
-	// playerSelection = playerSelection.toLowerCase();
+document.getElementById("rock").addEventListener("click", () => {
+	const playerSelection = "rock";
 	const computerSelection = getComputerChoice();
-	console.log(playerSelection, computerSelection);
-	console.log(playRound(playerSelection, computerSelection));
-}
+
+	document.getElementById("result").innerText = playRound(playerSelection, computerSelection);
+});
+
+document.getElementById("paper").addEventListener("click", () => {
+	const playerSelection = "paper";
+	const computerSelection = getComputerChoice();
+
+	document.getElementById("result").innerText = playRound(playerSelection, computerSelection);
+});
+
+document.getElementById("scissors").addEventListener("click", () => {
+	const playerSelection = "scissors";
+	const computerSelection = getComputerChoice();
+
+	document.getElementById("result").innerText = playRound(playerSelection, computerSelection);
+});
